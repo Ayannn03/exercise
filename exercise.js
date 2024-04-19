40fb36c6c9d0c8299f1b678eba785accb8aa0d5f
Ian Reniel R. Rey <127392923+Ayannn03@users.noreply.github.com>
2024-04-19 20:16:14 +0800

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,41 @@
+async function main() {
+    const student = await getStudentAsync(1);
+    console.log('Student: ', student);
+    if (student.isEnrolled) {
+      const courses = await getCourseListAsync();
+      console.log('Course List: ', courses);
+      await sendNotificationAsync(student.email, courses);
+      console.log('Notification sent...');
+    }
+  }
+  
+  async function getStudentAsync(id) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({
+          id: 1,
+          name: 'Ian Reniel R. Rey',
+          isEnrolled: true,
+          email: 'ianreneilrey25@gmial.com',
+        });
+      }, 4000);
+    });
+  }
+  
+  async function getCourseListAsync() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(['course1', 'course2']);
+      }, 4000);
+    });
+  }
+  
+  async function sendNotificationAsync(email, courses) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 4000);
+    });
+  }
+  
+  main();
